a1ac18b8966bf4a85a6f3d72b1aa8c78332a6a1b
denekm <mrtdeneke@gmail.com>
2021-12-18 19:27:28 -0800

    problem 2

--- js/app.js
+++ js/app.js
@@ -30,8 +30,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let multiply = a * b;
+  let message = `The product of ${a} and ${b} is ${multiply}.`;
+  return[multiply, message];
 }
+let multiplyReturn = multiply(5,9);
+console.log(multiplyReturn);
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
